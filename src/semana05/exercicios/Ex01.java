5ecf50d83b11af73972a557b41d5d49483f2c641
Tro Armen <troarmen@gmail.com>
2023-03-21 16:41:47 -0300

    semana 05 ex01

---
+++ src/semana05/exercicios/Ex01.java
@@ -0,0 +1,31 @@
+package semana05.exercicios;
+
+import java.util.Scanner;
+
+public class Ex01 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner (System.in);
+
+        System.out.println("Comprimento aquário:(cm) ");
+        double comprimento = sc.nextDouble();
+        System.out.println("Altura aquário:(cm) ");
+        double altura = sc.nextDouble();
+        System.out.println("Largura aquário:(cm) ");
+        double largura = sc.nextDouble();
+        System.out.println("Temperatura ambiente: (C⁰)");
+        double temperaturaAmbiente = sc.nextDouble(); 
+        System.out.println("Temperatura desejada:(C⁰) ");
+        double temperaturaDesejada = sc.nextDouble();
+
+        double volume = (comprimento * altura * largura) / 1000;
+        double potenciaTermostato  = (volume*0.05*(temperaturaAmbiente-temperaturaDesejada));
+        double filtragemPorHora = volume * 3;
+
+        System.out.println("\nVolume do aquário:" + volume);
+        System.out.println("A potência do termostato é: " + potenciaTermostato);
+        System.out.println("A filtragem por hora é: "+ filtragemPorHora);
+    }
+}
+
+
+	
